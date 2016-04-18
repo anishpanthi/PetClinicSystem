e11f2d14d90d6590b29de50241c8a7cbc3be81b6
anishpanthi <anish.panthi@gmail.com>
2016-04-17 21:11:03 -0500

    Spring Security Implemented

--- src/main/java/com/amt/petclinic/controller/HomeController.java
+++ src/main/java/com/amt/petclinic/controller/HomeController.java
@@ -1,9 +1,17 @@
 package com.amt.petclinic.controller;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import javax.annotation.Resource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -11,6 +19,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.amt.petclinic.dao.FeedbackDAO;
@@ -30,29 +39,46 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-//	public ModelAndView home(Locale locale, Model model) {
-		public ModelAndView home() {
+	@RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+	// public ModelAndView home(Locale locale, Model model) {
+	public ModelAndView home(Locale locale) {
 		// logger.info("Welcome home! The client locale is {}.", locale);
-		// Date date = new Date();
-		// DateFormat dateFormat =
-		// DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG,
-		// locale);
-		// String formattedDate = dateFormat.format(date);
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		String formattedDate = dateFormat.format(date);
 		// model.addAttribute("serverTime", formattedDate);
-		
+
 		ModelAndView model = new ModelAndView();
+		model.addObject("serverTime", formattedDate);
 		model.addObject("title", "Spring Security Hello World");
 		model.addObject("message", "This is welcome page!");
 		model.setViewName("home");
 		return model;
-		
-//		return "home";
+
+		// return "home";
 	}
-	
+
+	/*
+	 * @RequestMapping(value = "/login", method = RequestMethod.GET) public
+	 * String loginPage(Model model) { return "login"; }
+	 */
+
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String loginPage(Model model) {
-		return "login";
+	public ModelAndView login(@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout) {
+
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", "Invalid username and password!");
+		}
+
+		if (logout != null) {
+			model.addObject("msg", "You've been logged out successfully.");
+		}
+		model.setViewName("login");
+
+		return model;
+
 	}
 
 	@RequestMapping(value = "/feedback", method = RequestMethod.GET)
@@ -75,19 +101,39 @@ public class HomeController {
 
 	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
 	public ModelAndView adminPage() {
+
 		ModelAndView model = new ModelAndView();
-		model.addObject("title", "Spring Security Hello World");
-		model.addObject("message", "This is protected page - Admin Page!");
+		model.addObject("title", "Spring Security Custom Login Form");
+		model.addObject("message", "This is protected page!");
 		model.setViewName("admin");
+
 		return model;
+
 	}
 
-	@RequestMapping(value = "/dba**", method = RequestMethod.GET)
-	public ModelAndView dbaPage() {
+	/*
+	 * @RequestMapping(value = "/dba**", method = RequestMethod.GET) public
+	 * ModelAndView dbaPage() { ModelAndView model = new ModelAndView();
+	 * model.addObject("title", "Spring Security Hello World");
+	 * model.addObject("message", "This is protected page - Database Page!");
+	 * model.setViewName("admin"); return model; }
+	 */
+
+	// for 403 access denied page
+	@RequestMapping(value = "/403", method = RequestMethod.GET)
+	public ModelAndView accesssDenied() {
+
 		ModelAndView model = new ModelAndView();
-		model.addObject("title", "Spring Security Hello World");
-		model.addObject("message", "This is protected page - Database Page!");
-		model.setViewName("admin");
+
+		// check if user is login
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (!(auth instanceof AnonymousAuthenticationToken)) {
+			UserDetails userDetail = (UserDetails) auth.getPrincipal();
+			model.addObject("username", userDetail.getUsername());
+		}
+
+		model.setViewName("403");
 		return model;
+
 	}
 }
