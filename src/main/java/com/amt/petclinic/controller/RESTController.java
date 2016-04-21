46e87560d2bfda875175099c990a5de4080dfb21
anishpanthi <anish.panthi@gmail.com>
2016-04-21 02:36:24 -0500

    Test Classes and Packages Removed.

--- src/main/java/com/amt/petclinic/controller/RESTController.java
+++ src/main/java/com/amt/petclinic/controller/RESTController.java
@@ -12,56 +12,37 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.amt.petclinic.domain.Doctor;
 import com.amt.petclinic.domain.Feedback;
+import com.amt.petclinic.domain.Owner;
 import com.amt.petclinic.service.DoctorService;
 import com.amt.petclinic.service.FeedbackService;
+import com.amt.petclinic.service.OwnerService;
 import com.amt.petclinic.test.Country;
 
 @Component
 @RestController
 public class RESTController {
-	
+
 	@Autowired
 	private FeedbackService feedbackService;
-	
+
 	@Autowired
 	private DoctorService doctorService;
 
-	@RequestMapping(value="/api/getAllComments", method=RequestMethod.GET, headers="Accept=application/json")
-	public List<Feedback> getAllFeedbacks(){
+	@Autowired
+	OwnerService ownerService;
+
+	@RequestMapping(value = "/api/getAllComments", method = RequestMethod.GET, headers = "Accept=application/json")
+	public List<Feedback> getAllFeedbacks() {
 		return feedbackService.findAll();
 	}
-	
-	@RequestMapping(value="/api/getAllDoctors", method=RequestMethod.GET, headers="Accept=application/json")
-	public List<Doctor> getAllDoctors(){
+
+	@RequestMapping(value = "/api/getAllDoctors", method = RequestMethod.GET, headers = "Accept=application/json")
+	public List<Doctor> getAllDoctors() {
 		return doctorService.findAll();
 	}
 
-	@RequestMapping(value = "/api/getCountries/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
-	public Country getCountryById(@PathVariable int id) {
-		List<Country> listOfCountries = new ArrayList<Country>();
-		listOfCountries = createCountryList();
-		for (Country country : listOfCountries) {
-			if(country.getId()==id){
-				return country;
-			}
-		}
-		return null;
-	}
-
-	public List<Country> createCountryList() {
-		Country nepal = new Country(1, "Nepal");
-		Country china = new Country(2, "Bangladesh");
-		Country pakistan = new Country(3, "Pakistan");
-		Country bhutan = new Country(4, "Bhutan");
-		Country srilanka = new Country(5, "Sri Lanka");
-
-		List<Country> listOfCountries = new ArrayList<Country>();
-		listOfCountries.add(nepal);
-		listOfCountries.add(pakistan);
-		listOfCountries.add(srilanka);
-		listOfCountries.add(china);
-		listOfCountries.add(bhutan);
-
-		return listOfCountries;
+	@RequestMapping(value = "/api/getAllOwners", method = RequestMethod.GET, headers = "Accept=application/json")
+	public List<Owner> getAllOwners() {
+		return ownerService.findAll();
 	}
 }
